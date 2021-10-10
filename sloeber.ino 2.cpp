5c03f5de3b047b8097c176d518f2a17c38e5cdd5
Steffen Gregorius <steffen.gregorius@gmail.com>
2021-10-10 21:25:36 +0200

    updated jsons

---
+++ sloeber.ino 2.cpp
@@ -0,0 +1,24 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2021-10-10 01:38:11
+
+#include "Arduino.h"
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <string>
+#include <NimBLEDevice.h>
+#include "src/SparkButtonHandler.h"
+#include "src/SparkDataControl.h"
+#include "src/SparkDisplayControl.h"
+#include "src/SparkLEDControl.h"
+
+void setup() ;
+void loop() ;
+
+#include "Ignitron.ino"
+
+
+#endif
