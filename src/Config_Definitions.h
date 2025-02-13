f14e2dde279f04d148f664b24b4f9812ad380e25
Steffen Gregorius <steffen.gregorius@gmail.com>
2025-02-13 23:50:01 +0100

    version increase

--- src/Config_Definitions.h
+++ src/Config_Definitions.h
@@ -25,7 +25,7 @@ using namespace std;
 #endif
 
 // Software version
-const string VERSION = "1.8.2";
+const string VERSION = "1.8.3";
 
 // Battery indicator
 // Note: This battery is for the Ignitron controller, not the Spark amp.
