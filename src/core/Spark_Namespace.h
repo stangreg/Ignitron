482cbc7a911dc638d6006e8eaa7c44e7c63285d1
Steffen Gregorius <steffen.gregorius@gmail.com>
2025-09-06 12:59:52 +0200

    Add ifndef statements in Spark_Namespace.h to ensure proper header guard structure

--- src/core/Spark_Namespace.h
+++ src/core/Spark_Namespace.h
@@ -1,4 +1,11 @@
+#ifndef SPARK_NAMESPACE_H
+#define SPARK_NAMESPACE_H
+
+#include <vector>
+
 namespace Spark {
     using byte = uint8_t;
     using ByteVector = std::vector<byte>;
 }
+
+#endif // SPARK_NAMESPACE_H
